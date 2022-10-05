674c55d8e6f1dfcca3f93f17b6901f25fafdd946
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-10-04 23:31:35 -0300

    Passando imagem pra uma variável

--- Expo/FirstAppReactCursoUDMFG/App.js
+++ Expo/FirstAppReactCursoUDMFG/App.js
@@ -4,6 +4,7 @@ import { View, Text, Image  } from 'react-native';
 class App extends Component{
     render() {
         let nome = 'Dhionathan';
+        let imagemGalaxia = 'https://conteudo.imguol.com.br/c/noticias/2f/2021/08/02/imagem-meramente-ilustrativa-da-galaxia-de-andromeda-1627924758757_v2_900x506.jpg.webp';
         return (
             <View>
                 <Text>Testando React!!!</Text>
@@ -13,7 +14,7 @@ class App extends Component{
                     Sujeito Programador
                 </Text>
                 <Image
-                    source={{ uri: 'https://conteudo.imguol.com.br/c/noticias/2f/2021/08/02/imagem-meramente-ilustrativa-da-galaxia-de-andromeda-1627924758757_v2_900x506.jpg.webp' }}
+                    source={{ uri: imagemGalaxia}}
                     style={{ width: 300, height: 300 }}
                 />
                 <Text>{nome}</Text>
